a2faa73ff9422182c34e4f6dd8b9ce0a26aa44ff
jve89 <johanvanerkel@gmail.com>
2025-08-26 09:24:40 +0000

    chore: remove Vercel/Neon/Turso and serverless APIs

--- client/api/env-db.ts
+++
@@ -1,9 +0,0 @@
-// client/api/env-db.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-export const config = { runtime: 'nodejs' }
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  const url = process.env.DATABASE_URL || ''
-  let host = ''
-  try { host = new URL(url).hostname } catch {}
-  res.json({ has_DATABASE_URL: Boolean(url), host })
-}
--- client/api/ping.ts
+++
@@ -1,6 +0,0 @@
-// client/api/ping.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-export const config = { runtime: 'nodejs' }
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.status(200).send('ok')
-}
